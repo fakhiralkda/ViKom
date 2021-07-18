dc7cbd8c3f1530ce8325387e1047ff5f24a05c8a
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2021-07-18 13:15:50 +0700

    cawd250

--- kom.sh
+++ kom.sh
@@ -11,7 +11,7 @@ aria2c --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-tracker-connect-time
     --peer-id-prefix=-qB4350- --user-agent=qBittorrent/4.3.5 --peer-agent=qBittorrent/4.3.5 \
     --max-overall-upload-limit=0 --max-concurrent-downloads=7 --max-overall-download-limit=0 \
     --seed-time=2 --seed-ratio=1.0 --follow-torrent=true --split=10 \
-    https://transfer.sh/1Oy4iYe/onejav.com_hmn014_1.torrent
+    https://transfer.sh/1KmIO88/onejav.com_cawd250_1.torrent
 
 FILEPATH1=$(find . -type f -iname "*.mp4")
 FILEPATH2=$(find . -type f -iname "*.mkv")
@@ -27,3 +27,7 @@ echo "::endgroup::"
 
 [[ -n "$FILEPATH1" ]] && du -sh "$NAME1"-Comp.mp4 && curl -k -F "file=@${NAME1}-Comp.mp4" https://api.bayfiles.com/upload
 [[ -n "$FILEPATH2" ]] && du -sh "$NAME2"-Comp.mkv && curl -k -F "file=@{$NAME2}-Comp.mkv" https://api.bayfiles.com/upload
+
+echo "========================================"
+echo "Done ree"
+echo "Happy watching"
