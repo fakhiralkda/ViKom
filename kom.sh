8d7a5612faadb57bafce1e2bcc1ee8268c9e9166
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2022-03-20 06:12:29 +0700

    also reduce fps to 30

--- kom.sh
+++ kom.sh
@@ -6,11 +6,11 @@ echo "::endgroup::"
 
 echo "::group::Download"
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n\n' ',')
-aria2c https://transfer.sh/iVHafO/psy.MOV
+aria2c https://transfer.sh/UbsXdY/rbd841.mp4
 
 export CURL_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
 
-FILEPATH1=$(find . -type f -iname "*.MOV")
+FILEPATH1=$(find . -type f -iname "*.mp4")
 FILEPATH2=$(find . -type f -iname "*.mkv")
 
 [[ -n "$FILEPATH1" ]] && FILENAME1=$(basename -- "$FILEPATH1") && NAME1="${FILENAME1%.*}"
@@ -18,7 +18,7 @@ FILEPATH2=$(find . -type f -iname "*.mkv")
 echo "::endgroup::"
 
 echo "::group::Kompres"
-[[ -n "$FILEPATH1" ]] && ffmpeg -i $FILEPATH1 -c:v libx264 -preset medium -tune film -crf 20 -vf scale=720:-2 -c:a copy "$NAME1"-Comp.mp4
+[[ -n "$FILEPATH1" ]] && ffmpeg -loglevel warning -i $FILEPATH1 -filter:v fps=fps=30 -c:v libx264 -preset medium -tune film -crf 23 -vf scale=-2:480 -c:a copy "$NAME1"-Comp.mp4
 [[ -n "$FILEPATH2" ]] && ffmpeg -loglevel warning -i $FILEPATH2 -c:v libx264 -preset medium -tune film -crf 23 -vf scale=-2:480 -c:a copy "$NAME2"-Comp.mkv
 echo "::endgroup::"
 
